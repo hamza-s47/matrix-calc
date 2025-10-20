9e2ea1e9bd57843014d9cb1ddc7f97e52915b70d
Hamza <hamza.siddiqui4747@gmail.com>
2025-10-21 02:00:02 +0900

    arithmetic ops done!

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import numpy as np
-import operations.arithmetic as ar
+# import operations.arithmetic as ar
 
 mat1=np.array([[1,2], [3,4]])
-mat2=np.array([[6,3], [7,8,4]])
+mat2=np.array([[6,3,6], [7,8,6]])
+vect=np.array([1,2])
 
 # print(mat2.ndim)
 # print(np.matmul(mat1, mat2))
 
-print(ar.addition(mat1, mat2))
+# print(ar.addition(mat1, mat2))
+
+print(mat1+vect)
--- operations/arithmetic.py
+++ operations/arithmetic.py
@@ -1,11 +1,87 @@
 import numpy as np
 
-# Addition
-def addition(arr1, arr2):
-    try:
-        if arr1.shape == arr2.shape:
-            return(arr1+arr2)
-        else:
-            raise ValueError("Shape of Array not matched!")
-    except Exception as e:
-        return f"Error in addition: {e}"
+class MatrixArithmetic:
+    def __init__(self, arr1, arr2=None):
+        self.arr1 = arr1
+        self.arr2 = arr2
+
+    
+    # Addition
+    def addition(self):
+        try:
+            return self.arr1+self.arr2
+        except Exception as e:
+            return f"Error in Addition: {e}"
+
+    # Subtraction
+    def subtraction(self):
+        try:
+            return self.arr1-self.arr2
+        except Exception as e:
+            return f"Error in Subtraction: {e}"
+
+    # Element-wise Multiplication
+    def multiplication(self):
+        try:
+            return self.arr1*self.arr2
+        except Exception as e:
+            return f"Error in Element-wise Multiplication: {e}"
+
+    # Dot Product
+    def dot_product(self):
+        try:
+            if self.arr1.shape[1] == self.arr2.shape[0]:
+                return (np.dot(self.arr1, self.arr2))
+            else:
+                raise ValueError(f"Shape mismatch: Array 1 shape: {self.arr1.shape}, Array 2 shape: {self.arr2.shape}")
+        except Exception as e:
+            return f"Error in Dot Product: {e}"
+
+    # Scalar Multiplication
+    def s_multiplication(self, n):
+        try:
+            return self.arr1*n
+        except Exception as e:
+            return f"Error in Scalar Multiplication: {e}"
+
+    # Square Root
+    def sqrt(self):
+        try:
+            if np.any(self.arr1<0)
+                raise ValueError("Square root of negative numbers is not allowed.")
+            return (np.sqrt(self.arr1))
+        except Exception as e:
+            return f"Error in Square Root: {e}"
+        
+    # Sin
+    def sin(self):
+        try:
+            return (np.sin(self.arr1))
+        except Exception as e:
+            return f"Error in Sin: {e}"
+        
+    # Cos
+    def cos(self):
+        try:
+            return (np.cos(self.arr1))
+        except Exception as e:
+            return f"Error in Cos: {e}"
+        
+    # Log
+    def log(self):
+        try:
+            if np.any(self.arr1 <= 0):
+                raise ValueError("Logarithm undefined for Zero or Negative numbers.")
+            return (np.log(self.arr1))
+        except Exception as e:
+            return f"Error in Log: {e}"
+        
+    # Exponential
+    def exp(self):
+        try:
+            result = np.exp(self.arr1)
+            if np.any(np.isinf(result)):
+                raise OverflowError("Overflow encountered in exponential.")
+            return result
+        except Exception as e:
+            return f"Error in Exp: {e}"
